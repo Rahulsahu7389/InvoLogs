1808e7d0d3e6bdc41e2009494626909876aee890
Rahulsahu7389 <rahulsahuppd@gmail.com>
2025-12-30 13:20:39 +0530

    fixed name in dashboard

--- frontend/src/services/firebase.ts
+++ frontend/src/services/firebase.ts
@@ -4,13 +4,7 @@ import {getFirestore} from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDR8iUKGH5xSNxxadHXURLww4svFlGyUWA",
-  authDomain: "notes-app-1f946.firebaseapp.com",
-  databaseURL: "https://notes-app-1f946-default-rtdb.firebaseio.com",
-  projectId: "notes-app-1f946",
-  storageBucket: "notes-app-1f946.firebasestorage.app",
-  messagingSenderId: "211368205758",
-  appId: "1:211368205758:web:1cb0b877198349474badef"
+  
 };
 
 
